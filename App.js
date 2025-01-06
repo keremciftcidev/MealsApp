e9499e05f3f4d8746c21ceb682c63c934af68c59
Kerem Ciftci <krmciftci17@gmail.com>
2025-01-06 16:03:45 +0300

    Styling Screen Headers and Backgrounds

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator}from "@react-navigation/native-stack"
-import MealsCategoriesScreen from './screens/MealsOverviewScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsCategoriesScreen from "./screens/MealsOverviewScreen";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" backgroundColor="black" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
-          <Stack.Screen name='MealsOverview' component={MealsCategoriesScreen}/>
+        <Stack.Navigator
+          screenOptions={{
+            title: "All Categories",
+            headerStyle: { backgroundColor: "#816363" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#f3e5e5" },
+          }}
+        >
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsCategoriesScreen}
+            options={{ title: "Meals Overview" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
@@ -23,7 +34,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    backgroundColor: "#303031", 
+    flex: 1,
+    backgroundColor: "#303031",
   },
 });
